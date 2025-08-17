ccfa55b8a74013a0c439f14d3ce31568f7614908
arpnghosh <arpnghosh@protonmail.com>
2025-08-18 02:36:19 +0530

    feat(server): handle container creation requests for existing projects

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,14 +15,6 @@ const terminalWebSocketServer = new WebSocketServer({ server });
 
 const docker = new Docker();
 
-// TODO: app.get("/projects")
-
-// TODO: app.get("/projects/:projectId")
-
-// TODO: app.delete("/projects/:projectId")
-
-// TODO: watch file tree changes -> ui rendering
-
 interface serverConfigInterface {
   PORT: number;
   CLIENT_URL: string;
@@ -39,9 +31,12 @@ app.post("/create-new-project", async (req: Request, res: Response) => {
   try {
     const id = uuid4();
     const projectId = `project_${id}`;
-    const volumeName = `volume_${projectId}`;
-    const containerName = `container_${projectId}`;
+    const volumeName = `volume_${id}`;
+    const containerName = `container_${id}`;
     const imageName = "codebox-image";
+    console.log(volumeName);
+    console.log(containerName);
+    console.log(projectId);
 
     await docker.createVolume({ Name: volumeName });
 
@@ -58,7 +53,7 @@ app.post("/create-new-project", async (req: Request, res: Response) => {
         Binds: [`${volumeName}:/home/codebox/app`],
         AutoRemove: false,
         PortBindings: {
-          "5173/tcp": [{ HostPort: "5173" }],
+          "5173/tcp": [{ HostPort: "0" }],
         },
       },
       Cmd: ["/bin/bash", "-c", "tail -f /dev/null"],
@@ -67,13 +62,78 @@ app.post("/create-new-project", async (req: Request, res: Response) => {
     await projectContainer.start();
 
     res.status(200).json({
-      message: "Project created successfully",
+      message: "Container for project created successfully",
       projectId,
       containerId: projectContainer.id,
     });
   } catch (error) {
     console.error("Error creating project: ", error);
     res.status(500).json({ message: "Failed to create new project. " });
+  }
+});
+
+// TODO: app.get("/projects")
+// TODO: app.get("/projects?sort=oldest")
+// TODO: app.delete("/projects/:projectId")
+// TODO: watch file tree changes -> ui rendering
+
+const existsVolume = async (volumeName: string): Promise<boolean> => {
+  const volumes = await docker.listVolumes();
+  return volumes.Volumes.some((vol) => vol.Name == volumeName);
+};
+
+const isActiveContainer = async (containerName: string): Promise<boolean> => {
+  const containers = await docker.listContainers();
+  return containers.some((conInfo) =>
+    conInfo.Names.some((name) => name.includes(containerName)),
+  );
+};
+
+app.post("/projects/:projectId", async (req: Request, res: Response) => {
+  const { projectId } = req.params;
+  const id: string[] = projectId.split("_");
+  const uuid: string = id[1];
+  const volumeName = `volume_${uuid}`;
+  const containerName = `container_${uuid}`;
+  const imageName = "codebox-image";
+
+  try {
+    if (!(await existsVolume(volumeName))) {
+      res.status(404).json({ message: "Volume does not exist" });
+    }
+    if (await isActiveContainer(containerName)) {
+      res.status(200).json({ message: "container is active" });
+    }
+    const projectContainer = await docker.createContainer({
+      User: "codebox",
+      Image: imageName,
+      name: containerName,
+      Tty: true,
+      AttachStdin: true,
+      AttachStdout: true,
+      AttachStderr: true,
+      OpenStdin: true,
+      HostConfig: {
+        Binds: [`${volumeName}:/home/codebox/app`],
+        AutoRemove: false,
+        PortBindings: {
+          "5173/tcp": [{ HostPort: "0" }],
+        },
+      },
+      Cmd: ["/bin/bash", "-c", "tail -f /dev/null"],
+    });
+
+    await projectContainer.start();
+
+    res.status(200).json({
+      message: "container started again....",
+      projectId,
+      containerId: projectContainer.id,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Could not complete your request ${error}` });
   }
 });
 
